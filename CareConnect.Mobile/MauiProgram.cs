9ebfe302b822cb005006988a573ee54eb75547b0
Leonel Mendes <leonelftacisco@hotmail.com>
2026-06-21 18:22:03 +0100

    chore: Add fonts in projetc and update some line in loginscreen

--- CareConnect.Mobile/MauiProgram.cs
+++ CareConnect.Mobile/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using DotNet.Meteor.HotReload.Plugin;
 
 namespace CareConnect.Mobile;
 
@@ -9,10 +10,17 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+#if DEBUG
+            .EnableHotReload()
+#endif
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Inter_18pt-Bold.ttf", "InterBold");
+                fonts.AddFont("Inter_18pt-Semibold.ttf", "InterSemiBold");
+                fonts.AddFont("Inter_18pt-Regular.ttf", "InterRegular");
+                fonts.AddFont("Inter_18pt-Medium.ttf", "InterMedium");
 			});
 
 #if DEBUG
