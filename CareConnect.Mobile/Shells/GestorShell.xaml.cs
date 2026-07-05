a86197250a2d6174222cca50fa6dbfd9a9a4cee0
Leonel Francisco <leonelftacisco@hotmail.com>
2026-07-05 20:40:21 +0100

    Fix(mobile): Correcao do erro que impedia a inicialização do app, estana no xaml, um binding incorreto

--- CareConnect.Mobile/Shells/GestorShell.xaml.cs
+++ CareConnect.Mobile/Shells/GestorShell.xaml.cs
@@ -1,3 +1,5 @@
+using CareConnect.Mobile.Views.Shared;
+
 namespace CareConnect.Mobile.Shells;
 
 public partial class GestorShell : Shell
@@ -5,5 +7,10 @@ public partial class GestorShell : Shell
 	public GestorShell()
 	{
 		InitializeComponent();
-	}
+
+        Routing.RegisterRoute("UtentesView", typeof(UtentesView));
+        Routing.RegisterRoute("DetalheUtenteView", typeof(DetalheUtenteView));
+        Routing.RegisterRoute("PerfilView", typeof(PerfilView));
+        Routing.RegisterRoute("AdicionarUtenteView", typeof(AdicionarUtenteView));
+    }
 }
